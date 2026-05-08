b1321bea18ebce493a174873b2a746eb29356eeb
Victor <vichoara2425@gmail.com>
2026-05-08 15:20:57 -0500

    commit 08-05-2026 1520

---
+++ js/tailwindconfig.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+            theme: {
+                extend: {
+                    fontFamily: {
+                        montserrat: ['Montserrat', 'sans-serif'],
+                        dm2: ['Cairo', 'serif'],
+                        dm: ['Rajdhani', 'sans-serif'],
+                    }
+                }
+            }
+        }
